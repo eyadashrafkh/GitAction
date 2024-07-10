6e014bb74e28501237f1a89a2b977781d3616dc1
Iyad ashraf <eyad.ashraf941@gmail.com>
2024-07-10 18:06:41 +0300

    test

--- TestJavaFormat.java
+++ TestJavaFormat.java
@@ -1,4 +1,4 @@
 public class TestJavaFormat {
-  @SuppressWarnings("unused")
-  private static final String HELLO_WORLD = "Hello, world!";
+    @SuppressWarnings("unused")
+    private static final String HELLO_WORLD = "Hello, world!";
 }
